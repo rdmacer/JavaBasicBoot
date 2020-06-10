0805a91be4e15c54463fe1f94574e41d268ebc76
Ricardo Macedo <rmacedo@zagile.com>
2020-06-10 15:08:49 -0500

    JavaBasicBoot: change7

--- ModuloVideoteca/src/test/java/PeliculaTest.java
+++ ModuloVideoteca/src/test/java/PeliculaTest.java
@@ -3,15 +3,29 @@ import org.junit.Test;
 public class PeliculaTest {
   @Test
   public void test_Pelicula1() {
-    System.out.println("Iniciando test_Libro1");
+    System.out.println("Iniciando test_Pelicula1");
     Pelicula test = new Pelicula();
     System.out.println("La pelicula encontrada es: " + test.retornaNombrePeliculaPorId(1));
   }
 
   @Test
+  public void test_Pelicula2() {
+    System.out.println("Iniciando test_Pelicula2");
+    Pelicula test = new Pelicula();
+    System.out.println("La pelicula encontrada es: " + test.retornaNombrePeliculaPorId(2));
+  }
+
+  @Test
   public void test_Pelicula3() {
-    System.out.println("Iniciando test_Libro3");
+    System.out.println("Iniciando test_Pelicula3");
     Pelicula test = new Pelicula();
     System.out.println("La pelicula encontrada es: " + test.retornaNombrePeliculaPorId(3));
+  }
+
+  @Test
+  public void test_Pelicula4() {
+    System.out.println("Iniciando test_Pelicula4");
+    Pelicula test = new Pelicula();
+    System.out.println("La pelicula encontrada es: " + test.retornaNombrePeliculaPorId(4));
   }
 }
